469999aaa93cbaa0e6dc24ad074e0475ab25ddf8
IMAMx39 <imammaassou1@gmail.com>
2023-07-19 13:52:28 +0200

    add img to home page

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  @var FormBuilder $form
+ * @var FormBuilder $form
  * @var HomeController $user
  */
 
@@ -11,13 +11,26 @@ use Core\Form\FormBuilder;
 ?>
 
 <div class="mb-4">
-    <header class="container  px-4 px-lg-5" >
+    <div class="container  px-4 px-lg-5">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center" ">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <img class="text-center" src="assets/img/img2.jpg" style=" border-radius: 50%;  width: 20rem;height: 20rem; margin-left: 10rem; " alt="">
+                    <hr class="my-4"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mb-4">
+    <header class="container  px-4 px-lg-5">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <div class="site-heading col-md-10 col-lg-8 col-xl-7" style="    display: inline;">
                         <h1 style="margin-bottom: 1.5rem">
-                           <?php echo 'Welcome to my blog '.$user?->getPseudo() ?>
+                            <?php echo 'Welcome to my blog ' . $user?->getPseudo() ?>
                         </h1>
                     </div>
                 </div>
@@ -33,16 +46,18 @@ use Core\Form\FormBuilder;
 
                 <p>
                     Bienvenu !
-                    Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt.
+                    Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus,
+                    minus sunt.
                     Numquam at quae voluptatum in officia voluptas voluptatibus, minus!
                 </p>
 
-                <hr class="my-4" />
+                <hr class="my-4"/>
                 <h3 class="text-center" style="text-decoration: underline;">
-                    <a class="btn btn-primary" role="button" href="assets/downloads/" download="IMAMOSx95.pdf">Téléchargez mon CV !</a>
+                    <a class="btn btn-primary" role="button" href="assets/downloads/CV.pdf" download="IMAMOSx95.pdf">Téléchargez
+                        mon CV !</a>
                 </h3>
 
-                <hr class="my-4" />
+                <hr class="my-4"/>
                 <p>Vous souhaitez me contacter ? Remplissez le formulaire ci-dessous, et à très vite !</p>
             </div>
         </div>
@@ -56,7 +71,7 @@ use Core\Form\FormBuilder;
         <?php echo $form->row('username'); ?>
         <?php echo $form->row('email'); ?>
         <?php echo $form->row('subject'); ?>
-        <button type="submit"  value="submit" class="btn btn-primary mt-4 me-lg-4">Envoyer</button>
+        <button type="submit" value="submit" class="btn btn-primary mt-4 me-lg-4">Envoyer</button>
         <?php echo $form->end(); ?>
     </div>
 </div>
